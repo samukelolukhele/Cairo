93ae3b061b960d2c6e681d94dba4e9c3b267f615
samukelolukhele <samukelo.business@gmail.com>
2023-12-03 13:51:41 +0200

    FEAT: Background overlay added & fixes to mobile styling.

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -20,63 +20,54 @@ const Cart = ({ setIsOpen, isOpen }: Props) => {
   }
 
   return (
-    // <div>
-    //   {isOpen && (
-    //     <div
-    //       className="fixed w-screen h-screen top-0 right-0 left-0 bottom-0 bg-black opacity-50 cursor-pointer z-40"
-    //       onClick={() => setIsOpen(false)}
-    //     />
-    //   )}
-    // <Transition
-    //   show={isOpen}
-    //   enter="transition-expand ease-in-out duration-500"
-    //   enterFrom="opacity-0 w-[100px] md:w-0 pointer-events-none z-[-99]"
-    //   enterTo="opacity-100 w-[330px] md:w-96 z-50"
-    //   leave="transition-expand ease-in-out duration-300"
-    //   leaveFrom="opacity-100 w-[330px] md:w-96 z-50"
-    //   leaveTo="opacity-0 w-[100px] md:w-64 pointer-events-none z-[-99]"
-    // >
-    <Transition appear show={isOpen}>
-      <Transition.Child
-        className={`fixed p-4 top-0 right-0 bg-black flex flex-col items-center gap-6 h-screen overflow-y-auto z-50`}
-        enter="ease-in-out duration-700"
-        enterFrom="translate-x-full opacity-0"
-        enterTo="translate-x-0 opacity-100"
-        leave="ease-in-out duration-700"
-        leaveFrom="translate-x-0 opacity-100"
-        leaveTo="translate-x-full opacity-0"
-      >
-        <div className="flex w-full relative">
-          <p className="w-full text-3xl text-gray-400 font-thin">Cart</p>
-          <BsX
-            className="text-3xl absolute top-0 cursor-pointer right-0 hover:bg-brand duration-300 rounded-full"
-            onClick={() => setIsOpen(false)}
-          />
-        </div>
-        {cartItems.length != 0 ? (
-          <div className="flex flex-col border-t-[0.5px] border-gray-300 py-8 gap-4 w-full">
-            <CartItem />
-            <div className="w-full border-t-[1px] border-white py-2 flex justify-between gap-4 text-lg">
-              <p className="font-semibold">Subtotal</p>
-              <p className="font-light">${cartTotal}</p>
-            </div>
+    <div className="absolute">
+      {isOpen && (
+        <div
+          className="fixed w-screen h-screen top-0 right-0 left-0 bottom-0 bg-black opacity-50 cursor-pointer z-40"
+          onClick={() => setIsOpen(false)}
+        />
+      )}
+      <Transition appear show={isOpen}>
+        <Transition.Child
+          className={`fixed p-4 top-0 right-0 bg-black flex flex-col w-[330px] md:w-96 items-center gap-6 h-screen overflow-y-auto z-50`}
+          enter="ease-in-out duration-700"
+          enterFrom="translate-x-full opacity-0"
+          enterTo="translate-x-0 opacity-100"
+          leave="ease-in-out duration-700"
+          leaveFrom="translate-x-0 opacity-100"
+          leaveTo="translate-x-full opacity-0"
+        >
+          <div className="flex w-full relative">
+            <p className="w-full text-3xl text-gray-400 font-thin">Cart</p>
+            <BsX
+              className="text-3xl absolute top-0 cursor-pointer right-0 hover:bg-brand duration-300 rounded-full"
+              onClick={() => setIsOpen(false)}
+            />
           </div>
-        ) : (
-          <p className="text-2xl text-center font-thin my-auto">
-            Your cart is empty
-          </p>
-        )}
-        {cartItems.length > 0 && (
-          <button
-            className="w-full bg-none  rounded py-4 bg-brand hover:text-black font-light text-sm"
-            onClick={handleCheckout}
-          >
-            Checkout
-          </button>
-        )}
-      </Transition.Child>
-    </Transition>
-    //</div>
+          {cartItems.length != 0 ? (
+            <div className="flex flex-col border-t-[0.5px] border-gray-300 py-8 gap-4 w-full">
+              <CartItem />
+              <div className="w-full border-t-[1px] border-white py-2 flex justify-between gap-4 text-lg">
+                <p className="font-semibold">Subtotal</p>
+                <p className="font-light">${cartTotal}</p>
+              </div>
+            </div>
+          ) : (
+            <p className="text-2xl text-center font-thin my-auto">
+              Your cart is empty
+            </p>
+          )}
+          {cartItems.length > 0 && (
+            <button
+              className="w-full bg-none  rounded py-4 bg-brand hover:text-black font-light text-sm"
+              onClick={handleCheckout}
+            >
+              Checkout
+            </button>
+          )}
+        </Transition.Child>
+      </Transition>
+    </div>
   );
 };
 
